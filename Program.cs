828c9f5297866ac699f80e66f5c6d28d0b74735c
Kareman 17295774 <Kareman.17295774@compit.aun.edu.eg>
2022-01-31 14:45:12 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               sub sub=new sub();
+            Console.WriteLine(sub.subtract(9, 6));
+        }
+    }
+}
---
+++ sub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class sub
+    {
+        public int subtract (int x,int y)
+        { return x- y; }
+
+    }
+}
